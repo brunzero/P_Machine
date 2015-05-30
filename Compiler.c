45159496c46866544e6db0ffc85a8a8e81ee20fa
brunzero <brunothebeast@gmail.com>
2015-05-30 15:56:02 -0400

    merged functions and added a few functions

--- Compiler.c
+++ Compiler.c
@@ -4,18 +4,17 @@
 #define MAX_STACK_HEIGHT 2000
 #define MAX_CODE_LENGTH 500
 #define MAX_LEXI_LEVELS 3
-
-int sp = 0;
-int bp = 1;
-int pc = 0;
-int ir = 0;
-int codeSize = 0;
-
-int code[MAX_STACK_HEIGHT];
-int buffer;
+#define NEXT_INSTRUCTION 3
 
 char* parseOP(int op);
 void printCode(int *codes);
+void fetchCode();
+void getInstruction();
+void fetchInstruction();
+int getBase(int level, int base);
+void lit(int pushValue);
+void initArray();
+void load();
 
 typedef struct instruction
 {
@@ -24,12 +23,48 @@ typedef struct instruction
     int m;
 }instruction;
 
+int sp = 0;
+int bp = 1;
+int pc = 0;
+int codeSize = 0;
+int code[MAX_CODE_LENGTH];
+int stack[MAX_STACK_HEIGHT];
+int buffer;
+instruction ir;
+FILE *codeFile;
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+    load();
+   // printCode(code);
+
+    while(!isDone())
+    {
+        fetchInstruction();
+    }
+
+    return 0;
+}
+
+void load()
+{
+    initArray();
+    stack[1] = 0;
+    stack[2] = 0;
+    stack[3] = 0;
+    fetchCode();
+    fclose(codeFile);
+}
 
 void fetchCode()
 {
     int i = 0;
-    FILE *fp = fopen("mcode.txt", "r");
-    while(fscanf(fp, "%d", &buffer)!=EOF)
+    codeFile = fopen("mcode.txt", "r");
+    while(fscanf(codeFile, "%d", &buffer)!=EOF)
     {
         code[i]=buffer;
         codeSize++;
@@ -61,6 +96,28 @@ void printCode(int *codes)
 
 void fetchInstruction()
 {
+    getInstruction();
+    pc += NEXT_INSTRUCTION;
+}
+
+void getInstruction()
+{
+    ir.op = code[pc];
+    ir.l = code[pc + 1];
+    ir.m =  code[pc + 2];
+    printf("%d,",code[pc]);
+    printf("%d,",code[pc + 1]);
+    printf("%d \n",code[pc + 2]);
+}
+
+//initializes stack and code arrays
+void initArray()
+{
+    int i;
+    for(i = 0; i < MAX_STACK_HEIGHT; i ++)
+        stack[i] = NULL;
+    for(i = 0; i < MAX_CODE_LENGTH; i ++)
+        code[i] = NULL;
 
 }
 
@@ -104,11 +161,47 @@ char *parseOP(int op)
     return opword;
 }
 
-int main(int argc, char *argv[])
+int getBase(int level, int base)
 {
-    fetchCode();
-    printCode(code);
+    int i;
+    for(i = level; i > 0; i --)
+    {
+        base = stack[base + 2];
+    }
+    return base;
+}
 
+void lit ()
+{
+    sp ++;
+    stack[sp] = ir.m;
+}
 
+//01
+void jmp ()
+{
+    pc = M * NEXT_INSTRUCTION;
+}
+
+//03
+void lod ()
+{
+    sp++;
+    stack[sp] = stack[getBase(ir.l, bp) + ir.m];
+}
+
+//04
+void sto()
+{
+    stack[getBase(ir.l, bp) + ir.m] = stack[sp];
+    sp = sp-1;
+}
+
+int isDone ()
+{
+    if(!code[pc])
+        return 1;
     return 0;
 }
+
+
